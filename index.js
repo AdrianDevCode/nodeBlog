6512dc07126cc35d6bfd90029ea21547d3a1c20f
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-05-17 11:43:26 -0400

    put in Post function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const express = require("express");
 const util = require("util");
+const bodyParser = require('body-parser');
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const readFile = util.promisify(fs.readFile);
 
@@ -16,6 +19,25 @@ app.get("/blog/:id", (req, res) => {
     readFile("./blogData.json").then((data) => {
         const dataBlog = JSON.parse(data);
         dataBlog[id] ? res.json(dataBlog[id]) : res.json({});
+    });
+});
+
+app.post("/blog/new", urlencodedParser, (req, res) => {
+    readFile("./blogData.json").then((data) => {
+
+        let dataBlog = JSON.parse(data);
+        let newId = Object.keys(dataBlog).length + 1;
+        let newPost = {
+            id: newId,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            date: req.body.date,
+            title: req.body.title,
+            content: req.body.content
+        };
+        dataBlog[newId] = newPost;
+        fs.writeFile("./blogData.json", JSON.stringify(dataBlog, null, ' '),() => {res.send(newPost);
+        });
     });
 });
 
